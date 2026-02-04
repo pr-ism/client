ebfae7db1291d643a6956b7a0a37690552709e2f
apptie <appti@naver.com>
2026-02-04 16:02:59 +0900

    Fix: OAuth를 백엔드로 직접 리다이렉트하도록 수정

--- app/page.tsx
+++ app/page.tsx
@@ -145,7 +145,8 @@ function HomePageContent() {
   }, [openModal]);
 
   const handleSocialLogin = useCallback((provider: 'kakao' | 'google') => {
-    const url = `/oauth2/authorization/${provider}`;
+    const backendUrl = process.env.NEXT_PUBLIC_STATS_SERVER_URL || 'http://localhost:8081';
+    const url = `${backendUrl}/oauth2/authorization/${provider}`;
     window.location.href = url;
   }, []);
 
